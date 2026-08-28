80d170315dcc173a4ddd66086555c707e1ca1626
Matias Passarino <matiaspassarino@users.noreply.github.com>
2026-08-28 08:54:00 -0300

    Fix routing for sub-apps in Vite and Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rewrite-middleware',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/pages/industrias-sur') {
+            req.url = '/pages/industrias-sur/';
+          }
+          if (req.url === '/pages/estudio-juridico') {
+            req.url = '/pages/estudio-juridico/';
+          }
+          next();
+        });
+      }
+    }
+  ],
   server: { port: 5173, open: true },
   build: {
     rollupOptions: {
